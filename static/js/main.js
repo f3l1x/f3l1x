af256ab619a036261498589dc11a57e529d26729
Milan Felix Šulc <rkfelix@gmail.com>
2017-06-06 20:29:19 +0200

    Build blogposts, improve homepage

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,16 @@
 $(function () {
-    // f3l1x..
+    // Code highlighting
+    $('pre code').each(function (i, block) {
+        var $block = $(block);
+        $.each($block.prop("classList"), function (i, c) {
+            $block.removeClass(c);
+            c = c.replace('language-', '');
+            if (!$block.hasClass(c)) $block.addClass(c);
+        });
+
+        hljs.highlightBlock(block);
+    });
+
+    // Tooltip
+    $('[data-toggle="tooltip"]').tooltip();
 });
