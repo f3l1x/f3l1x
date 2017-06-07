af1ae6a86c7178031928f41519c710205da75bbe
Milan Felix Šulc <rkfelix@gmail.com>
2017-06-07 10:22:37 +0200

    Giignore: untrack bundled assets

--- static/dist/bundle.js
+++
@@ -1,1 +0,0 @@
-$(function(){$("pre code").each(function(a,l){var o=$(l);$.each(o.prop("classList"),function(a,l){o.removeClass(l),l=l.replace("language-",""),o.hasClass(l)||o.addClass(l)}),hljs.highlightBlock(l)}),$('[data-toggle="tooltip"]').tooltip()});
