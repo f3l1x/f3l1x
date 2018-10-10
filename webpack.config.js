75a9c2c7e3684d7d71e4e7a854b5773bcbfd7fe7
Milan Felix Šulc <rkfelix@gmail.com>
2018-10-10 19:04:13 +0200

    Assets: upgrade deps, use Terser optimization, use cache-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,28 @@
 // Webpack
 const webpack = require("webpack");
 
-// Plugins
+// Webpack plugins
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+// Webpack abilities
+const WEBPACK_REPORT = process.env.WEBPACK_REPORT || false;
+
+// Vue
+const VUE_VERSION = require('vue/package.json').version;
+const VUE_LOADER_VERSION = require('vue-loader/package.json').version;
 
 // Node
 const path = require('path');
 
 // Utils
 const isDev = process.env.NODE_ENV !== 'production';
+
+// Config
+const ROOT_PATH = __dirname;
+const CACHE_PATH = ROOT_PATH + '/temp/webpack';
 
 module.exports = {
     entry: {
@@ -31,8 +42,15 @@ module.exports = {
                     loaders: {
                         'scss': 'vue-style-loader!css-loader!sass-loader',
                         'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
-                    }
-                }
+                    },
+                    cacheDirectory: path.join(CACHE_PATH, 'vue-loader'),
+                    cacheIdentifier: [
+                        process.env.NODE_ENV || 'development',
+                        webpack.version,
+                        VUE_VERSION,
+                        VUE_LOADER_VERSION,
+                    ].join('|'),
+                },
             },
             {
                 test: /\.tsx?$/,
@@ -44,10 +62,11 @@ module.exports = {
             },
             {
                 test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: "babel-loader"
-                }
+                exclude: path => /node_modules/.test(path) && !/\.vue\.js/.test(path),
+                loader: 'babel-loader',
+                options: {
+                    cacheDirectory: path.join(CACHE_PATH, 'babel-loader'),
+                },
             },
             {
                 test: /\.css$/,
@@ -59,7 +78,7 @@ module.exports = {
                             importLoaders: 1,
                             sourceMap: true,
                             minimize: true,
-                            discardComments: { removeAll: true },
+                            discardComments: {removeAll: true},
                         }
                     }
                 ]
@@ -74,7 +93,7 @@ module.exports = {
                             importLoaders: 1,
                             sourceMap: true,
                             minimize: true,
-                            discardComments: { removeAll: true },
+                            discardComments: {removeAll: true},
                         }
                     },
                     {
@@ -96,10 +115,10 @@ module.exports = {
                             importLoaders: 1,
                             sourceMap: true,
                             minimize: true,
-                            discardComments: { removeAll: true },
+                            discardComments: {removeAll: true},
                         }
                     },
-                    { loader: 'sass-loader' },
+                    {loader: 'sass-loader'},
                 ]
             },
             {
@@ -127,15 +146,23 @@ module.exports = {
     resolve: {
         extensions: ['.ts', '.js', '.vue', '.json'],
         alias: {
-            'vue$': 'vue/dist/vue.esm.js'
+            'vue$': 'vue/dist/vue.esm.js',
+            '@': path.resolve(__dirname, 'assests/main/vue'),
         },
     },
     performance: {
         hints: false
     },
+    node: {fs: 'empty'},
     devtool: '#eval-source-map',
     plugins: [
+        // prevent pikaday from including moment.js
+        new webpack.IgnorePlugin(/moment/, /pikaday/),
+
+        // enable vue-loader to use existing loader rules for other module types
         new VueLoaderPlugin(),
+
+        // extract css
         new MiniCssExtractPlugin({
             filename: "[name].css",
             chunkFilename: "[id].css"
@@ -143,17 +170,50 @@ module.exports = {
     ],
 };
 
+if (WEBPACK_REPORT) {
+    module.exports.plugins.push(
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+            generateStatsFile: true,
+            openAnalyzer: false,
+            reportFilename: path.join(CACHE_PATH, 'webpack-report/index.html'),
+            statsFilename: path.join(CACHE_PATH, 'webpack-report/stats.json'),
+        })
+    );
+}
+
 if (!isDev) {
-    module.exports.devtool = '#source-map'
+    module.exports.devtool = '#source-map';
+    module.exports.optimization = {
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    cache: `${CACHE_PATH}/webpack/terser`,
+                    parallel: true,
+                    ecma: 8,
+                    warnings: false,
+                    parse: {},
+                    compress: {},
+                    mangle: true, // Note `mangle.properties` is `false` by default.
+                    module: false,
+                    output: null,
+                    toplevel: false,
+                    nameCache: null,
+                    ie8: false,
+                    keep_classnames: undefined,
+                    keep_fnames: false,
+                    safari10: false
+                }
+            })
+        ]
+    };
+
     // http://vue-loader.vuejs.org/en/workflow/production.html
     module.exports.plugins = (module.exports.plugins || []).concat([
         new webpack.DefinePlugin({
             'process.env': {
                 NODE_ENV: '"production"'
             }
-        }),
-        new webpack.LoaderOptionsPlugin({
-            minimize: true
         }),
         new OptimizeCSSAssetsPlugin({
             cssProcessorOptions: {
@@ -162,19 +222,5 @@ if (!isDev) {
                 },
             },
         }),
-        new UglifyJsPlugin({
-            uglifyOptions: {
-                ecma: 8,
-                warnings: false,
-                output: {
-                    comments: false,
-                    beautify: false,
-                },
-                toplevel: false,
-                nameCache: null,
-                ie8: false,
-                safari10: false,
-            }
-        })
     ])
 }
